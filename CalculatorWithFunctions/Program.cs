0aa42cbc4d05c218c702c8260523cff052299077
Tanja Levcenko <tanjox78@gmail.com>
2023-12-14 10:32:35 +0300

    add my first app

---
+++ CalculatorWithFunctions/Program.cs
@@ -0,0 +1,33 @@
+﻿int a = 5;
+int b = 6;
+
+
+Add(a, b);
+Add(10, 100);
+Add(56583, 658686);
+
+
+Multiply(a, b);
+Multiply(10, 100);
+Multiply(156583, 1);
+
+
+static void Add(int x, int y)
+{
+    Console.WriteLine($"{x} + {y} = {x + y}");
+}
+
+static void Multiply(int x, int y)
+{
+    Console.WriteLine($"{x} * {y} = {x * y}");
+}
+
+static void Substract(int x, int y) // x - y
+{
+    //your code
+}
+
+static void Drivide(int x, int y) //x / y
+{
+    //your code
+}
